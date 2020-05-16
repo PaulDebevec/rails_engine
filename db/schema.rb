64ab34f92463d5238088a67eeceecb8219515275
Paul Debevec <paulmdebevec@gmail.com>
2020-05-16 12:31:27 -0600

    Add test and model for Customer including validations

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2020_05_16_173834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "customers", force: :cascade do |t|
+    t.text "first_name"
+    t.text "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.text "name"
