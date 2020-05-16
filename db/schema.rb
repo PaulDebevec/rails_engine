bb9402507e7d58d39f7ef54f0c07b4df4a5dac78
Paul Debevec <paulmdebevec@gmail.com>
2020-05-16 12:31:07 -0600

    Add test and model for Item including validations

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 2020_05_16_173834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+
+  create_table "items", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.text "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "merchants", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", null: false
