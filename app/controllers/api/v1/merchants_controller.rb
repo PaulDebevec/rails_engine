d91d13f9c01ea09f991c27512f7f3952d0c822ab
Paul Debevec <paulmdebevec@gmail.com>
2020-05-18 16:50:28 -0600

    Refactor merchant controller. Add serialized_merchants helper method.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,27 +1,36 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: MerchantSerializer.new(Merchant.all).serialized_json
+    merchants = Merchant.all
+    render json: serialized_merchants(merchants).serialized_json
   end
 
   def show
-    render json: MerchantSerializer.new(Merchant.find(params[:id])).serialized_json
+    merchant = Merchant.find(params[:id])
+    render json: serialized_merchants(merchant).serialized_json
   end
 
   def create
-    render json: MerchantSerializer.new(Merchant.create(merchant_params)).serialized_json
+    new_merchant = Merchant.create(merchant_params)
+    render json: serialized_merchants(new_merchant).serialized_json
   end
 
   def update
-    render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params)).serialized_json
+    updated_merchant = Merchant.update(params[:id], merchant_params)
+    render json: serialized_merchants(updated_merchant).serialized_json
   end
 
   def destroy
-    render json: MerchantSerializer.new(Merchant.destroy(params[:id]))
+    destroy_merchant = Merchant.destroy(params[:id])
+    render json: serialized_merchants(destroy_merchant)
   end
 
   private
 
   def merchant_params
     params.permit(:name)
+  end
+
+  def serialized_merchants(merchant_data)
+    MerchantSerializer.new(merchant_data)
   end
 end
